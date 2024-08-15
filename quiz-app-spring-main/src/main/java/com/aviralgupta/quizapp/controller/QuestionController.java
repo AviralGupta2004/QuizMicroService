98c1fb34d7d28b407306c26f9b3aaa7c6760688c
AviralGupta2004 <aviralgupta852004@gmail.com>
2024-08-15 13:56:51 +0530

    Add files via upload

---
+++ quiz-app-spring-main/src/main/java/com/aviralgupta/quizapp/controller/QuestionController.java
@@ -0,0 +1,34 @@
+package com.aviralgupta.quizapp.controller;
+
+
+import com.aviralgupta.quizapp.model.Question;
+import com.aviralgupta.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
+        return  questionService.addQuestion(question);
+    }
+
+}
